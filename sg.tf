63b5de503f88b691fcea24a95a15ae1d1cde6c52
babjan <babjansb43@gmail.com>
2023-04-14 15:01:39 +0530

    modified vpc id

--- sg.tf
+++ sg.tf
@@ -1,14 +1,14 @@
 resource "aws_security_group" "bastion" {
   name        = "bastion-sg"
   description = "connecting admin through ssh"
-  vpc_id      = vpc-0603de69d0195f915
+  vpc_id      = vpc-0b2cc03ae520e8690
 
   ingress {
     description      = "sg"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["103.110.170.85/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
